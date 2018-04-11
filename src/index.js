0c23b3f35f2764e9cabe4b01dc91c9abbd7c45d0
subhrag <subhrab2@gmail.com>
2018-04-11 14:51:23 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 let sass = require('./sass/style.scss');
 let skill = require('./js/skills.js');
+//let resize = require('./js/scroll.js');
---
+++ src/js/load.js
@@ -0,0 +1,5 @@
+let projects = document.getElementsByClassName()[0];
+
+document.getElementById("project__more").onclick = function() {
+
+}
